b0c626e9fd0fa41e967a258bdbcd0680e08ee300
Frank Ha <hmhai0395@gmail.com>
2022-06-19 23:27:17 +0700

    Python Panda correlation.

--- cleaning_data.py
+++ cleaning_data.py
@@ -5,11 +5,21 @@ df = pd.read_csv('dirtydata.csv')
 print('\nBefore cleaning up:\n')
 print(df)
 
-x = df["Calories"].mode()[0]
-print(x)
+# x = df["Calories"].mode()[0]
+# print(x)
 
-df["Calories"].fillna(x, inplace=True)
+# df["Calories"].fillna(x, inplace=True)
 
 
-print('\nAfter cleaning up:\n')
+# print('\nAfter cleaning up:\n')
+# print(df)
+
+df['Date'] = pd.to_datetime(df['Date'])
+df.dropna(subset=['Date'], inplace=True)
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.drop(x, inplace=True)
+df.drop_duplicates(inplace=True)
+
 print(df)
+print(df.duplicated())
---
+++ correlation/data_correlation.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+file_path = 'data.csv'
+
+df = pd.read_csv(file_path)
+
+print(df.head())
+
+
+print(df.corr())
